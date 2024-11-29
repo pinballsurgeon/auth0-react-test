c51646a800299cdf9d5b96b94be87f45462c4f50
Dan Ehlers <pinballsurgeon@gmail.com>
2024-11-29 16:10:19 -0500

    dehls - science experiment 9120-049-93

--- src/pages/coordinate-page.js
+++ src/pages/coordinate-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PageLayout } from "../../components/page-layout";
-import CoordinateSpaceWithSearch from '../../components/visualization/CoordinateSpaceWithSearch';
+import CoordinateSpaceWithSearch from '../components/visualization/CoordinateSpace';
 
 export const CoordinateSpacePage = () => {
   return (
