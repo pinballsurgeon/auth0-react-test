7fc6a6d8935d459a76cd942661ba21ed0a99cf03
Dan Ehlers <pinballsurgeon@gmail.com>
2024-11-29 08:38:41 -0500

    dehls - correct coordinate space

--- src/pages/coordinate-page.js
+++ src/pages/coordinate-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { PageLayout } from "../../components/page-layout";
-import CoordinateSpace from '../../components/visualization/CoordinateSpace';
+import { PageLayout } from "../components/page-layout";
+import CoordinateSpace from '../components/visualization/CoordinateSpace';
 
 export const CoordinateSpacePage = () => {
   return (
@@ -15,4 +15,4 @@ export const CoordinateSpacePage = () => {
   );
 };
 
-export default CoordinateSpacePage;
+// export default CoordinateSpacePage;
