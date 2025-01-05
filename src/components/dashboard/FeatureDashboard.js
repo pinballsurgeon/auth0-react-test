cc43ec525bfdde141e8e91f65a8df1b935a052ae
Dan Ehlers <pinballsurgeon@gmail.com>
2025-01-05 14:25:30 -0500

    dehls - wow now

--- src/components/dashboard/FeatureDashboard.js
+++ src/components/dashboard/FeatureDashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { generateDomainItems, MODELS } from '../../services/devPanelService';
+import { generateDomainItems } from '../../services/devPanelService';
+import { MODELS } from '../../services/llmProvider';
 import { testGCPConnection } from '../../services/gcpService'; 
 
 // Simple SVG icons as components
